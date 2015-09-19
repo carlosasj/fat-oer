7b4c0f47a45159399c2e99f6c5695d349c71f129
Carlos Schneider <carlosaschjr@gmail.com>
2015-09-19 13:57:47 -0300

    Coloca a nova navbar e sidebar

--- assets/js/language_selector.js
+++ assets/js/language_selector.js
@@ -2,31 +2,30 @@ function refresh_i18n() {
     $("[data-i18n]").each(function () {
         var prop = $(this).data('i18n');
         $(this).text($.i18n.prop(prop))
-    }
-    );
+    });
 }
 
-$('#languageSelector').click(function () {
-    changeLanguage($('#languageSelector').prop('checked') ? "en" : "pt");
-    refresh_i18n();
-}
-);
 
 // leave blank to use default language (en)
 function changeLanguage(lang) {
-    lang = lang || ''; // if no language is set, use default (en)
-
+    lang = lang || ""; // if no language is set, use default (en)
+    console.log("Lang >> "+lang);
     $.i18n.properties({
         name    : 'Messages',
         path    : 'languages/',
         mode    : 'both',
         language: lang,
         callback: refresh_i18n
-    }
-    );
+    });
 }
 
-// Initialize with default language
-changeLanguage();
+$(document).ready(function(){
+    // Initialize with default language
+    changeLanguage();
+    refresh_i18n();
 
-
+    $('#languageSelector').click(function () {
+        var sel = $('#languageSelector').prop('checked') ? "en" : "pt";
+        changeLanguage(sel);
+    });
+});
