33aade40e00501b3c035a7396f5153cb6cfe9516
Kasama <ralegro@bol.com.br>
2015-09-19 01:57:58 -0300

    added fully functional i18n support

---
+++ assets/js/language_selector.js
@@ -0,0 +1,32 @@
+function refresh_i18n() {
+    $("[data-i18n]").each(function () {
+        var prop = $(this).data('i18n');
+        $(this).text($.i18n.prop(prop))
+    }
+    );
+}
+
+$('#languageSelector').click(function () {
+    changeLanguage($('#languageSelector').prop('checked') ? "en" : "pt");
+    refresh_i18n();
+}
+);
+
+// leave blank to use default language (en)
+function changeLanguage(lang) {
+    lang = lang || 'en'; // if no language is set, use default (en)
+
+    $.i18n.properties({
+        name    : 'Messages',
+        path    : 'languages/',
+        mode    : 'both',
+        language: lang,
+        callback: refresh_i18n
+    }
+    );
+}
+
+// Initialize with default language
+changeLanguage();
+
+
