919a6299af4f13fa1acd6b5a3754bc4299449fec
Carlos Schneider <carlosaschjr@gmail.com>
2015-10-20 16:50:41 -0200

    Start create some graphs

--- assets/js/language_selector.js
+++ assets/js/language_selector.js
@@ -21,6 +21,26 @@ function changeLanguage(lang) {
         callback: refresh_i18n
     });
 }
+
+function updatePairText(id, value){
+    $('#'+id).val(value);
+}
+
+function updatePairSlider(id, value){
+    $('#'+id)[0].MaterialSlider.change(value);
+}
+
+function updateChart(id, value){
+    console.log($('#'+id)[0].data);
+    // var seg0 = $('#'+id)[0].segments[0].value;
+    // var seg1 = $('#'+id)[0].segments[1].value;
+    // var total = seg0+seg1;
+    //
+    // $('#'+id)[0].segments[0].value = total-value;
+    // $('#'+id)[0].segments[1].value = value;
+    // $('#'+id)[0].update();
+}
+
 $(document).ready(function(){
     // Initialize with default language
     changeLanguage();
@@ -31,12 +51,36 @@ $(document).ready(function(){
     });
 
     $('[data-control-pair][type="text"]').keyup(function updatePair() {
-        var pairID = $(this).data('control-pair')
-        $('#'+pairID)[0].MaterialSlider.change($(this).val());
+        var pairID = $(this).data('control-pair');
+        updatePairSlider(pairID, $(this).val());
+        updateChart('chart-memory-1', $(this).val());
     });
 
     $('[data-control-pair][type="range"]').mousemove(function updatePair() {
-        var pairID = $(this).data('control-pair')
-        $('#'+pairID).val($(this).val());
+        var pairID = $(this).data('control-pair');
+        updatePairText(pairID, $(this).val());
+        updateChart('chart-memory-1', $(this).val());
     });
+
+    var data = [
+        {
+            value: 1,
+            color: "#EEEEEE",
+            highlight: "#F2F2F2",
+            label: "Livre"
+        },
+        {
+            value: 1,
+            color:"#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Consumido"
+        },
+    ]
+    var context = $('#chart-memory-1').get(0).getContext("2d");
+    var chartMemory1 = new Chart(context).Doughnut(data,{
+        animateRotate : false,
+        tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %> MB",
+    });
+
+
 });
