db325abe679ce22d27d958e1cba7ea8ba3283296
Kasama <ralegro@bol.com.br>
2015-10-27 15:45:46 -0200

    working without color

--- assets/js/backend_to_frontend.js
+++ assets/js/backend_to_frontend.js
@@ -1,33 +1,52 @@
 var number_regex = /^[1-9]\d*$/;
 var fs = null;
 
-function add_fat_entry(fileName){
-    var entry = '<tr> <td class="fat-entry">' + fileName + '</td> </tr>';
-    $('#fat-container')[0].innerHTML += entry;
+function initialize_fat(numberOfBlocks){
+    var fat_container = $('#fat-container')[0];
+    fat_container.innerHTML = null;
+    for (var i = 0; i < numberOfBlocks; i++) {
+        var block = '<tr><td>'+i+'</td> <td id="fat-block-id-'+i+'" class="fat-entry">' + fs.fat.entries[i] + '</td></tr>';
+        fat_container.innerHTML += block;
+    }
 }
 
 function initialize_disk(numberOfBlocks){
-    var block = '<div class="block-cluster"></div> ';
     var disk_container = $('#disk-container')[0];
+    disk_container.innerHTML = null;
     for (var i = 0; i < numberOfBlocks; i++) {
+        var block = '<div id="block-id-'+i+'" class="block-cluster"></div> ';
         disk_container.innerHTML += block;
     }
 }
 
 function addFile(name, size, color){
-    fs.addFile(name, size, color);
+    var file = fs.addFile(name, size, color);
     add_file_list_entry(name);
-    update_fat();
-    update_disk();
+    update_fat(file);
+    //update_disk(file);
 }
 
-function update_fat(){
+function removeFile(fileName){
+    var file = fs.removeFile(fileName);
+    remove_file_list_entry(fileName);
+    update_fat(file);
+    //update_disk(file);
+}
+
+function update_fat(file){
     var fat = fs.fat;
-    var fatField = $('#')
+    file.getBlocks().forEach(function (b) {
+        var block = $('#fat-block-id-'+b)[0];
+        block.innerHTML = fs.fat.entries[b];
+    })
+}
+
+function remove_file_list_entry(fileName){
+    $('#file-list-entry-'+fileName).remove();
 }
 
 function add_file_list_entry(fileName){
-    var entry = "<tr> <td class=\"file-entry\"><a data-file=\"" +fileName+ "\" class=\"remove-file-button\" href=\"#\"><i class=\"material-icons\">close</i></a>"+ fileName +"</td></tr>";
+    var entry = "<tr id=\"file-list-entry-"+fileName+"\"> <td class=\"file-entry\"><a onclick=\"removeFile('"+fileName+"');\" data-file=\"" +fileName+ "\" class=\"remove-file-button\" href=\"#\"><i class=\"material-icons\">close</i></a>"+ fileName +"</td></tr>";
     $('#file-list-container')[0].innerHTML += entry;
 }
 
@@ -47,6 +66,7 @@ function initialize_apply_changes(){
 
     createFileSystem(blockSize, numberOfBlocks);
     initialize_disk(numberOfBlocks);
+    initialize_fat(numberOfBlocks);
 
     flipcard('#config-card');
 
@@ -65,8 +85,13 @@ function handle_apply_add_file(){
     var size = sizeField.val();
     //var color = colorField.???();
 
+    if (fs.fileList.findFileIndex(name) != -1){
+        alert($.i18n.prop('name-already-exists-error'));
+        return;
+    }
+
     if (name == "" || size == ""){
-        alert("vaitomarnocu");
+        alert($.i18n.prop('empty-field-error'));
         return;
     }
 
@@ -76,6 +101,6 @@ function handle_apply_add_file(){
     }
 
     addFile(name, size, "#000000");
-
+    //addFile(name, size, color);
 
 }
