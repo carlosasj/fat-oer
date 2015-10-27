f780233adbbb631dd04c02463421a212b41ee324
Kasama <ralegro@bol.com.br>
2015-10-27 13:58:49 -0200

    fixed index stuff and implemented basic integration

---
+++ assets/js/backend_to_frontend.js
@@ -0,0 +1,75 @@
+var number_regex = /^[1-9]\d*$/;
+var fs = null;
+
+function add_fat_entry(fileName){
+    var entry = '<tr> <td class="fat-entry">' + fileName + '</td> </tr>';
+    $('#fat-container')[0].innerHTML += entry;
+}
+
+function initialize_disk(numberOfBlocks){
+    var block = '<div class="block-cluster"></div> ';
+    for (var i = 0; i < numberOfBlocks; i++) {
+        $('#disk-container')[0].innerHTML += block;
+    }
+}
+
+function addFile(name, size, color){
+    fs.addFile(name, size, color);
+    add_file_list_entry(name);
+    update_fat();
+    update_disk();
+}
+
+function add_file_list_entry(fileName){
+    var entry = "<tr> <td class=\"file-entry\"><a data-file=\"" +fileName+ "\" class=\"remove-file-button\" href=\"#\"><i class=\"material-icons\">close</i></a>"+ fileName +"</td></tr>";
+    $('#file-list-container')[0].innerHTML += entry;
+}
+
+//TODO add and remove files from interface!
+
+function initialize_apply_changes(){
+    var blockSizeField = $("#disk-blocksize");
+    var numberOfBlocksField = $("#disk-blocknumber");
+
+    var blockSize = get_slider_value(blockSizeField);
+    var numberOfBlocks = numberOfBlocksField.val();
+
+    if (number_regex.exec(numberOfBlocks) == null){
+        alert($.i18n.prop('not-a-number-error'));
+        return;
+    }
+
+    createFileSystem(blockSize, numberOfBlocks);
+    initialize_disk(numberOfBlocks);
+
+    flipcard('#config-card');
+
+}
+
+function createFileSystem(blockSize, numberOfBlocks) {
+    fs = new FileSystem(blockSize, numberOfBlocks);
+}
+
+function handle_apply_add_file(){
+    var nameField = $('#add-file-name');
+    var sizeField = $('#add-file-size');
+    //var colorField = ???;
+
+    var name = nameField.val();
+    var size = sizeField.val();
+    //var color = colorField.???();
+
+    if (name == "" || size == ""){
+        alert("vaitomarnocu");
+        return;
+    }
+
+    if (number_regex.exec(size) == null){
+        alert($.i18n.prop('not-a-number-error'));
+        return;
+    }
+
+    addFile(name, size, "#000000");
+
+
+}
