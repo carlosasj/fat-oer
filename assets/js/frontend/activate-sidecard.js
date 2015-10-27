35b513f3f9668f540e67ea0542b706e8dfb371b5
Carlos Schneider <carlosaschjr@gmail.com>
2015-10-27 00:41:15 -0200

    Hide sidecard

---
+++ assets/js/frontend/activate-sidecard.js
@@ -0,0 +1,14 @@
+$('#sidecardSelector').click(function () {
+    var value = $(this).is(':checked');
+    console.log(value);
+    if (value) {
+        $('.sidecard').addClass('show-sidecard');
+        $('.maincard').addClass('mdl-cell mdl-cell--9-col-desktop');
+        $('.maincard').removeClass('mdl-cell--12-col-desktop');
+    }
+    else {
+        $('.sidecard').removeClass('show-sidecard');
+        $('.maincard').removeClass('mdl-cell mdl-cell--9-col-desktop');
+        $('.maincard').addClass('mdl-cell--12-col-desktop');
+    }
+});
